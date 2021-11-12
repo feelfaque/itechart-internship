da8bd8a11609a9c7b3c006a9d0f5473e5bca6fc8
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2021-11-12 20:23:44 +0300

    [feedme] added Logo component

---
+++ cookbook/src/components/forms/sign-up-form/SignUpForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './SignUpForm.css';
+import LogoBig from "../../logo/LogoBig";
+
+
+function SignUpForm () {
+    return (
+        <form className="signup-form">
+            <LogoBig />
+        </form>
+    );
+}
+
+export default SignUpForm;
---
+++ cookbook/src/components/sign-up/SignUp.js
@@ -0,0 +1,12 @@
+import './SignUp.css';
+import SignUpForm from "../forms/sign-up-form/SignUpForm";
+
+function SignUp () {
+    return(
+        <div className="signup-page">
+            <div className="signup--background">
+            </div>
+            <SignUpForm />
+        </div>
+    );
+}
