546b4d15dc0dfb403dc6d13fb83fdf3e8ed29f8f
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2021-11-16 18:03:57 +0300

    [feedme] added react router

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,31 +1,21 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
+  Routes,
   Route
 } from "react-router-dom";
 
 import './App.css';
-import Home from "./components/pages/home/Home";
 import SignUp from "./components/pages/sign-up/SignUp";
 import SignIn from "./components/pages/sign-in/SignIn";
 
 export default function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route path"/signup">
-                <SignUp />
-            </Route>
-            <Route path="/login">
-                <SignIn />
-            </Route>
-        </Switch>
-      </Router>
+            <Routes>
+                <Route exact path="/"/>
+                <Route path="/signup" element={<SignUp />}/>
+                <Route path="/login" element={<SignIn />}/>
+            </Routes>
     </div>
   );
 }
